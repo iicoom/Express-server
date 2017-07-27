b2672c61f725fd4242cc762b28d4677cad78e8c9
Leo <asdfpeng@qq.com>
2017-07-27 11:24:36 +0800

    complete questions API

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.get('/',function(req,res){
 	res.send('Hello Express-api!')
 })
 
+var questions=[{
+	id:213,
+	status:444,
+	range:12
+},
+{
+	id:456,
+	status:678,
+	range:13
+}];
+//qustions API
+app.get('/questions',function(req,res){
+	res.status(200),
+	res.json(questions)
+})
+
 /*Express 内置的 express.static 可以方便地托管静态文件，
 例如图片、CSS、JavaScript 文件等*/
 // app.use(express.static(path.join(__dirname, 'public')));
