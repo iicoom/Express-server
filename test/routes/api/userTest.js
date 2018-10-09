402747a0f479b26c6ce38e5be57e3c8c88678910
Leo <asdfpeng@qq.com>
2018-10-09 10:31:17 +0800

    test params

--- test/routes/api/userTest.js
+++ test/routes/api/userTest.js
@@ -5,6 +5,8 @@
 var request = require('supertest');
 var app = require('../../../app');
 // console.log(app.address())
+
+// 用户注册
 describe('user regist test',function(){
   it('should return 手机号不能为空',function(done){
     request(app)
@@ -15,4 +17,20 @@ describe('user regist test',function(){
     })
     .end(done);
   })
+});
+
+// 用户登录
+describe('POST /api/session/login', function() {
+  it('responds with json', function(done) {
+    request(app)
+    .post('/api/session/login')
+    .send({mobile: '18231088178', password: '123456'})
+    .set('Accept', 'application/json')
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      console.log(res.body);
+      done();
+    });
+  });
 });
